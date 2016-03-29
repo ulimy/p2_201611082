b18b185b4223308b47152a76ad98d433e72203ff
ulimy <ulim130@naver.com>
2016-03-29 23:25:17 +0900

    201611082
    201611082

---
+++ w4Main.py
@@ -0,0 +1,11 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def makeSwirSquare(size,bigger,turns,sides,angle):
+    for i in range(sides):
+        if(i%2):
+            size=size+bigger
+            t1.fd(size)
+            t1.right(angle)
+makeSwirSquare(20,10,10,50,90)
+wn.exitonclick()
