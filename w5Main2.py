7d4584add853f1db45882cfdca834b59803ab2c2
ulimy <ulim130@naver.com>
2016-03-30 13:11:40 +0900

    201611082
    201611082

---
+++ w5Main2.py
@@ -0,0 +1,12 @@
+﻿marks=input("User input grade (0~100) : ")
+print "%d" %marks,"marks"
+if 90<=marks<=100:
+    print "%s" %"A"
+elif 80<=marks<90:
+    print "%s" %"B"
+elif 70<=marks<80:
+    print "%s" %"C"
+elif 60<=marks<50:
+    print "%s" %"D"
+else:
+    print "%s" %"F" 
