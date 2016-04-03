12b094de7c675be2a96346522df57f29d8d92877
ulimy <ulim130@naver.com>
2016-04-03 19:06:26 +0900

    가위바위보 homework

---
+++ w5MainHw.py
@@ -0,0 +1,22 @@
+﻿def rsp():
+    a=input('a!! r? s? p?:')
+    b=input('b!! r? s? p?:')
+    if a==b:
+        print ("draw")
+    elif a=='r':
+        if b=='s':
+            print ("a win")
+        elif b=='p':
+            print ("b win")
+    elif a=='s':
+        if b=='r':
+            print ("b win")
+        elif b=='p':
+            print ("a win")
+    elif a=='p':
+        if b=='r':
+            print ("a win")
+        elif b=='s':
+            print ("b win")
+rsp()
+wn.exitonclick
