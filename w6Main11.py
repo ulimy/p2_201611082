f1480439476f18ab75ae6edfe3bb2f65fe46e99e
ulimy <ulim130@naver.com>
2016-04-07 23:14:24 +0900

    윤년인지 맞추기

---
+++ w6Main11.py
@@ -0,0 +1,16 @@
+﻿def year():
+    year1=input("year?: ")
+    year=int(year1)
+    if (year %4==0) and (year %100!=0 or year %400==0):
+        print ("yes!!")
+    else:
+        print ("no!!")
+
+def lab6():
+	year()
+
+def main():
+	lab6()
+
+if _name_=="_main_":
+	main()
