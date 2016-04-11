194528104e05854d685222b1ea6a074ea8c992a7
ulimy <ulim130@naver.com>
2016-04-11 17:25:58 +0900

    4&5

---
+++ w6Main2.py
@@ -0,0 +1,17 @@
+﻿def sumList(aList):
+    x=list()
+    for i in range(0,aList):
+        if i%4==0 and i%5!=0:
+            x.append(i)
+    mysum=0
+    for i in range(0,len(x)):
+        mysum=mysum+x[i]
+    return mysum
+    
+def lab6():
+    aList=1000
+    labsum=sumList(aList)
+    print (labsum)
+    
+def main():
+    lab6()
