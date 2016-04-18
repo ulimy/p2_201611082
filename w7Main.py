e1efc35f4f15cabb250b2ada9a6b0f6d905ecc66
ulimy <ulim130@naver.com>
2016-04-18 17:06:19 +0900

    데이터구조-2,3

---
+++ w7Main.py
@@ -0,0 +1,30 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def drawSquareAtSave(size, pos):
+    t1.penup() 
+    t1.setpos(pos) 
+    tracks=list()
+    for i in range(0,4):
+        tracks.append(t1.pos())
+        t1.forward(size)
+        t1.right(90)
+    return tracks
+
+def drawSquareFrom():
+    t1.pendown()
+    tracks=((0,0), (100,0), (100,-100), (0,-100), (0,0))
+    for i in range(0,5):
+        t1.goto(tracks[i])
+    return tracks
+
+def lab7():
+    mytrack=drawSquareAtSave(100,(0,0))
+    print (mytrack)
+    t1.home()
+    t1.clear()
+    drawSquareFrom()
+    
+def main():
+    lab7()
