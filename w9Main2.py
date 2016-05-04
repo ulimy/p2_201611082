1793fef8b018cfdb123bf7ba2974dcde3e2b2943
ulimy <ulim130@naver.com>
2016-05-04 15:17:45 +0900

    경복궁입구에서 제일 가까운 지하철역 찾기

---
+++ w9Main2.py
@@ -0,0 +1,10 @@
+﻿Locations=tuple()
+Locations=[(37.576557,126.985470),(37.571617,126.976439),(37.574454,126.957917),(37.570172,126.983096,(37.570423,126.992178))]
+a=37.579618
+b=126.977041
+import math
+distance=list()
+for i in Locations:
+    distances=math.sqrt(a-i[0])**2+(b-i[1])**2
+    distance.append(distances)
+print (min(y))
