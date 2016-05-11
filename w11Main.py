5b5249ccc23f75e787292f0abfcef1145411038f
ulimy <ulim130@naver.com>
2016-05-11 15:46:34 +0900

    이벤트

---
+++ w11Main.py
@@ -0,0 +1,32 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+t2=turtle.Turtle()
+t2.penup()
+t2.setpos(-50,100)
+t2.pendown()
+t2.forward(100)
+def keyup():
+    t1.forward(50)
+def keydown():
+    t1.backward(50)
+def keyright():
+    t1.right(90)
+def keyleft():
+    t1.left(90)
+def mousegoto(x,y):
+    t1.setpos(x,y)
+    t1.pos=(x,y) 
+    if -50<x<50 and 95<y<105:
+        print ("Correct")
+def addkeys():
+    wn.onkey(keyup,"Up")
+    wn.onkey(keydown,"Down")
+    wn.onkey(keyright,"Right")
+    wn.onkey(keyleft,"Left")
+def addmouse():
+    wn.onclick(mousegoto)
+addkeys()
+addmouse() 
+wn.listen()
+turtle.mainloop()
