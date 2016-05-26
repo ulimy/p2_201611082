659fa879a3aa88563b9b159e53c19b9433437574
ulimy <ulim130@naver.com>
2016-05-26 15:11:49 +0900

    0525

---
+++ w12Main2.py
@@ -0,0 +1,48 @@
+﻿def h1():
+    myfile=open('output.txt','w')
+    line1='first line\n'
+    myfile.write(line1)
+    line2='\tsecond line\n'
+    myfile.write(line2)
+    line3='third'
+    myfile.write(line3)
+    myfile.close()
+    fin=open('output.txt','r')
+    fout=open('outputUpper.txt','w')
+    import time
+    for line in fin:
+        words=line.split()
+        if line.find('line')>=0:
+            msg='[YL edited {0}]'.format(time.strftime('%Y-%m-%d %H:%M:%S'))
+            fout.write(msg)
+        for word in words:
+            if word=='line':
+                word=word.upper()
+            fout.write(word)
+        fout.write('\n')
+    fin.close()
+    fout.close()
+
+def h2():
+    data=[1,2,3,4,5,6,7,8,9,10]
+    dataopen=open('dataopen.txt','w')
+    for i in data:
+        if i%2:
+            dataopen.write("{0}\t".format(i))
+        else:
+            dataopen.write("{0}\t\n".format(i))
+    dataopen.close() 
+
+def lab12(): 
+    h1() 
+    h2() 
+ 
+ 
+def main(): 
+    lab12() 
+ 
+if __name__=="__main__": 
+    main()  
+ 
+ 
+
