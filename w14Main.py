ba73e44bba00dec7d4f6a9ed374a2eb8fd514471
ulimy <ulim130@naver.com>
2016-06-01 23:13:57 +0900

    강아지 울음소리

---
+++ w14Main.py
@@ -0,0 +1,26 @@
+﻿class Dog(object):
+    def __init__(self,name):
+        self.name=name
+    def talk(self):
+        print ('my dog is',self.name,'mung mung')
+    
+class ShihTzuDog(object):
+    def __init__(self,name):
+        self.name=name
+    def talk(self):
+        print ('my dog is',self.name,'si si')
+        
+class Maltese(object):
+    def __init__(self,name):
+        self.name=name
+    def talk(self):
+        print ('my dog is',self.name,'mal mal')
+
+mydog=Dog('dog')
+mydog.talk()
+
+mydog=ShihTzuDog('ShihTzuDog')
+mydog.talk()
+
+mydog=Maltese('Maltese')
+mydog.talk()
